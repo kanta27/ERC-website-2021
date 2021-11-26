eb07f2b30725da0bef3efaaf278607588a31103d
Gade Chandramouli <chandramouli085@gmail.com>
2021-11-26 22:50:02 +0530

    mobile sidebar added

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,37 @@
 import styles from "./NavBar.module.scss"
 import Link from 'next/link'
+import { useState } from "react"
 
 const NavBar=()=>{
-    return (
-    <nav className={styles.navbar}>
-      <ul className={styles.list}>
-        <li className={styles.button}><Link href="/"><a>Home</a></Link></li>
-        <li className={styles.button}><Link href="/"><a>Events</a></Link></li>
-        <li className={styles.button}><Link href="/"><a>Team Details</a></Link></li>
-        <li className={styles.button}><Link href="/"><a>Contact Us</a></Link></li>
-      </ul>
-    </nav>
-    )
+  const [sidebar, setSidebar]= useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <ul className={styles.list}>
+          <li className={styles.button}><Link href="/"><a>Home</a></Link></li>
+          <li className={styles.button}><Link href="/"><a>Events</a></Link></li>
+          <li className={styles.button}><Link href="/"><a>Team Details</a></Link></li>
+          <li className={styles.button}><Link href="/"><a>Contact Us</a></Link></li>
+        </ul>
+      </nav> 
+      <div className={styles.navbar_mobile}>
+        <div className={styles.na}>
+          <button href="/" className={styles.menu_bars} onClick={showSidebar}>☰</button>
+        </div>
+        <nav className={sidebar ? styles.nav_menu_active : styles.nav_menu}>
+          <ul className={styles.nav_menu_items} onClick={showSidebar}>
+            <li className={styles.button}><Link href="/"><a>Close &times;</a></Link></li>
+            <li className={styles.button}><Link href="/"><a>Home</a></Link></li>
+            <li className={styles.button}><Link href="/"><a>Events</a></Link></li>
+            <li className={styles.button}><Link href="/"><a>Team Details</a></Link></li>
+            <li className={styles.button}><Link href="/"><a>Contact Us</a></Link></li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
 }
 
 export default NavBar
