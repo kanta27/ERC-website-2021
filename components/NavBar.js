9a5a9d0dd259c1713d0a5e79ba5a8b82091a8c6b
Electronics and Robotics Club, IIT Bombay <63636136+erciitb@users.noreply.github.com>
2023-07-21 19:04:50 +0530

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -24,11 +24,11 @@ const NavBar=()=>{
         <nav className={sidebar ? styles.nav_menu_active : styles.nav_menu}>
           <ul className={styles.nav_menu_items} onClick={showSidebar}>
             <li className={styles.buttons_view}><Link href="#"><a>Close &times;</a></Link></li>
-            <li className={styles.buttons_view}><Link href="https://itc.gymkhana.iitb.ac.in/erc-iitb/#home"><a>Home</a></Link></li>
+            <li className={styles.buttons_view}><Link href="https://erciitb.github.io/#home"><a>Home</a></Link></li>
             <li className={styles.buttons_view}><Link href="/blog"><a>Blog</a></Link></li>
-            <li className={styles.buttons_view}><Link href="https://itc.gymkhana.iitb.ac.in/erc-iitb/#events"><a>Events</a></Link></li>
-            <li className={styles.buttons_view}><Link href="https://itc.gymkhana.iitb.ac.in/erc-iitb/#ercteamdet"><a>Team Details</a></Link></li>
-            <li className={styles.buttons_view}><Link href="https://itc.gymkhana.iitb.ac.in/erc-iitb/#contact"><a>Contact Us</a></Link></li>
+            <li className={styles.buttons_view}><Link href="https://erciitb.github.io/#events"><a>Events</a></Link></li>
+            <li className={styles.buttons_view}><Link href="https://erciitb.github.io/#ercteamdet"><a>Team Details</a></Link></li>
+            <li className={styles.buttons_view}><Link href="https://erciitb.github.io/#contact"><a>Contact Us</a></Link></li>
           </ul>
         </nav>
       </div>
